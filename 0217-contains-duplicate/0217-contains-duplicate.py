2c333d0270759059b218ab5534f88237e3e235b5
anjaliravikumar111 <anjaliravikumar348@gmail.com>
2026-06-21 09:58:43 +0530

    Time: 24 ms (26.29%), Space: 25.9 MB (28.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+
+        visited = set()
+
+        for num in nums:
+            if num in visited:
+                return True
+
+            visited.add(num)
+
+        return False
