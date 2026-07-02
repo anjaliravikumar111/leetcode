c26b17438cd01a64362f098b66ae3ee2bad1458b
anjaliravikumar111 <anjaliravikumar348@gmail.com>
2026-07-02 21:13:13 +0530

    Time: 224 ms (97.56%), Space: 35.2 MB (60.63%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,19 @@
+from bisect import bisect_left
+
+class Solution:
+    def successfulPairs(self, spells, potions, success):
+
+        potions.sort()
+
+        n = len(potions)
+        ans = []
+
+        for spell in spells:
+
+            target = (success + spell - 1) // spell
+
+            idx = bisect_left(potions, target)
+
+            ans.append(n - idx)
+
+        return ans
