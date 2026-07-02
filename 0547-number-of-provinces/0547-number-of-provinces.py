8b66aced026272412dd8dee9513787a776144141
anjaliravikumar111 <anjaliravikumar348@gmail.com>
2026-07-02 20:49:18 +0530

    Time: 7 ms (73.05%), Space: 13.7 MB (18.66%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findCircleNum(self, isConnected):
+        n = len(isConnected)
+        visited = set()
+
+        def dfs(city):
+            for nei in range(n):
+                if isConnected[city][nei] == 1 and nei not in visited:
+                    visited.add(nei)
+                    dfs(nei)
+
+        provinces = 0
+
+        for i in range(n):
+            if i not in visited:
+                visited.add(i)
+                dfs(i)
+                provinces += 1
+
+        return provinces
