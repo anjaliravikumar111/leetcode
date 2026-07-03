0969ff9ec7c0a7c13ef26ab212277c2ff27de2b7
anjaliravikumar111 <anjaliravikumar348@gmail.com>
2026-07-03 19:44:34 +0530

    Time: 0 ms (100%), Space: 12.4 MB (63.23%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numTilings(self, n):
+        MOD = 10**9 + 7
+
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+
+        for i in range(4, n + 1):
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD
+
+        return dp[n]
